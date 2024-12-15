5e800625ddc70f1029a94b9d30ba5c4b7b4d432e
fathur-pr <132331379+fathur-pr@users.noreply.github.com>
2024-12-15 16:41:31 +0700

    Add files via upload

---
+++ bantuan.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Sistem Pakar Sapi</title>
+<meta http-equiv="Sapi-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" sapi="width=device-width, initial-scale=1, maximum-scale=1">
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+<body>
+<div class="wrap">
+	
+<!--main-->
+<div>
+
+<div class="Sapi">
+<div class="isi">
+
+<center><img class="profile-pic"  src="images/unpak.png" alt="" /></center><br>
+<p style="text-align: center; text-indent:0.5in;"> Tugas web ini dibuat untuk memenuhi tugas Akhir dari Mata kuliah Metode Penelitian  </p>
+<p style="text-align: center; text-indent:0.5in;">Muhamad Fathurohman	( 065115009 )</p>
+<br>
+
+</div>
+</div>
+
+</div>
+
+
+</div>
+</body>
+</html>
